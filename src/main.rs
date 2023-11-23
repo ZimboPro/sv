ba67ba7d0c5d3ea57ac38c97b494cf079b1612fe
ZimboPro <dejagerleslie@gmail.com>
2023-11-23 13:52:40 +0200

    Add extra checks

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
 mod open_api;
 mod terraform;
+mod util;
 
 use clap::Parser;
 
 use open_api::validate_open_api;
-use paris::{error, info};
+use openapiv3::{self, Operation};
+use paris::{error, warn};
 
 use std::path::PathBuf;
 // extern crate pretty_env_logger;
 // #[macro_use]
 // extern crate log;
-use terraform::validate_terraform;
+use terraform::{validate_terraform, APIPath};
+
+use crate::util::HttpMethod;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -50,16 +54,158 @@ fn main() -> anyhow::Result<()> {
     validating_path(&args.terraform)?;
     let merged_yaml_content = validate_open_api(api_path)?;
     let keys = validate_terraform(args.terraform)?;
+    // TODO rather check for path and attached arn key
+    let mut valid = true;
+    let doc: openapiv3::OpenAPI = serde_yaml::from_str(&merged_yaml_content)?;
     for key in keys {
-        let len = merged_yaml_content.matches(&key).count();
-        if len > 1 {
-            error!(
-                "The Lambda ARN placeholder '{}' is used {} times in in the OpenAPI docs",
-                key, len
-            );
+        if let Some(arn_key) = key.arn_template_key {
+            key.apis
+                .iter()
+                .for_each(|api| {
+                    match doc.paths.paths.get(&api.route) {
+                    Some(path_item) => match api.method {
+                        HttpMethod::Get => {
+                            if let Some(config) = &path_item.as_item().unwrap().get {
+                                if !validate_aws_api_gateway_integration(config, &key.key, &arn_key, &api) {
+                                    valid = false;
+                                }
+                            }
+                            else {
+                                valid = false;
+                                error!("The GET method is not defined for the path {} for the lambda {}", api.route, key.key);
+                            }
+                        }
+                        HttpMethod::Post => {
+                            if let Some(config) = &path_item.as_item().unwrap().post {
+                                if !validate_aws_api_gateway_integration(config, &key.key, &arn_key, &api) {
+                                    valid = false;
+                                }
+                            } else {
+                                valid = false;
+                                error!("The POST method is not defined for the path {} for the lambda {}", api.route, key.key);
+                            }
+                        }
+                        HttpMethod::Put => {
+                            if let Some(config) = &path_item.as_item().unwrap().put {
+                                if !validate_aws_api_gateway_integration(config, &key.key, &arn_key, &api) {
+                                    valid = false;
+                                }
+                            } else {
+                                valid = false;
+                                error!("The PUT method is not defined for the path {} for the lambda {}", api.route, key.key);
+                            }
+                        }
+                        HttpMethod::Delete => {
+                            if let Some(config) = &path_item.as_item().unwrap().delete {
+                                if !validate_aws_api_gateway_integration(config, &key.key, &arn_key, &api) {
+                                    valid = false;
+                                }
+                            } else {
+                                valid = false;
+                                error!(
+                                    "The DELETE method is not defined for the path {} for the lambda {}",
+                                    api.route, key.key
+                                );
+                            }
+                        }
+                        HttpMethod::Patch => {
+                            if let Some(config) = &path_item.as_item().unwrap().patch {
+                                if !validate_aws_api_gateway_integration(config, &key.key, &arn_key, &api) {
+                                    valid = false;
+                                }
+                            } else {
+                                valid = false;
+                                error!(
+                                    "The PATCH method is not defined for the path {} for the lambda {}",
+                                    api.route, key.key
+                                );
+                            }
+                        }
+                        HttpMethod::Options => {
+                            if let Some(config) = &path_item.as_item().unwrap().options {
+                                if !validate_aws_api_gateway_integration(config, &key.key, &arn_key, &api) {
+                                    valid = false;
+                                }
+                            } else {
+                                valid = false;
+                                error!(
+                                    "The OPTIONS method is not defined for the path {} for the lambda {}",
+                                    api.route, key.key
+                                );
+                            }
+                        }
+                        HttpMethod::Head => {
+                            if let Some(config) = &path_item.as_item().unwrap().head {
+                                if !validate_aws_api_gateway_integration(config, &key.key, &arn_key, &api) {
+                                    valid = false;
+                                }
+                            } else {
+                                valid = false;
+                                error!("The HEAD method is not defined for the path {} for the lambda {}", api.route, key.key);
+                            }
+                        }
+                        _ => todo!(),
+                    },
+                    None => {
+                        error!("The path {} is not defined in OpenAPi for the lambda {}", api.route, key.key);
+                    }
+                }
+        });
+            let len = merged_yaml_content.matches(&arn_key).count();
+            if len > 1 {
+                valid = false;
+                error!(
+                    "The Lambda ARN placeholder '{}' is used {} times in in the OpenAPI docs",
+                    arn_key, len
+                );
+            } else if len == 0 {
+                valid = false;
+                error!(
+                    "The Lambda ARN placeholder '{}' is not used in the OpenAPI docs",
+                    arn_key
+                );
+            }
         }
     }
-    info!("\nMake sure to check the JSON policy in either api_gateway.tf or the resources for the attached policy.");
-    println!("Done");
+    if !valid {
+        return Err(anyhow::anyhow!("Invalid Terraform and OpenAPI documents"));
+    }
+    println!("");
+    warn!("Make sure to check the JSON policy in either api_gateway.tf or the resources for the attached policy.");
     Ok(())
+}
+
+fn validate_aws_api_gateway_integration(
+    config: &Operation,
+    lambda_key: &str,
+    arn_key: &str,
+    api: &APIPath,
+) -> bool {
+    let mut valid = true;
+    match config.extensions.get("x-amazon-apigateway-integration") {
+        Some(aws) => {
+            match aws.get("uri") {
+                Some(uri) => {
+                    let uri_path = uri.as_str().unwrap();
+                    if !uri_path.contains(&arn_key) {
+                        valid = false;
+                        error!("The 'uri' doesn't contain the ARN placeholder '{}' in the 'x-amazon-apigateway-integration' extension for {} {} for the lambda {}", arn_key, api.method, api.route, lambda_key);
+                    }
+                    if uri_path.contains("state:action") {
+                        valid = false;
+                        error!("The 'uri' for {} {} is set up for step functions instead of the lambda {}", api.method, api.route, lambda_key);
+                    }
+                }
+                None => {
+                    valid = false;
+                    error!("The 'uri' doesn't exist in the 'x-amazon-apigateway-integration' extension for {} {} for the lambda {}", api.method, api.route, lambda_key);
+                }
+            }
+        }
+        None => {
+            valid = false;
+            error!("The 'x-amazon-apigateway-integration' extension doesn't exist for the {} {} for the lambda {}", api.method, api.route, lambda_key);
+        }
+    }
+    valid
 }
