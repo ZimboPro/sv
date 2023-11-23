dea1f8eb5b2aed0d73854fc5718a1089fabbc3ba
Leslie <dejagerleslie@gmail.com>
2023-11-23 20:21:07 +0200

    linting

--- src/open_api.rs
+++ src/open_api.rs
@@ -8,119 +8,119 @@ use sppparse::SparseRoot;
 use std::{ffi::OsStr, io::Read, path::PathBuf};
 
 pub fn validate_open_api(api_path: PathBuf) -> anyhow::Result<String> {
-    info!("Validating OpenAPI documents");
+  info!("Validating OpenAPI documents");
 
-    let mut files = find_files(api_path.as_path(), OsStr::new("yml"));
-    files.append(&mut find_files(api_path.as_path(), OsStr::new("yaml")));
-    let mut tags = Vec::new();
-    let mut valid = true;
-    for file in &files {
-        match SparseRoot::new_from_file(PathBuf::from_iter([
-            std::env::current_dir().unwrap(),
-            file.to_path_buf(),
-        ])) {
-            Ok(open_api_doc) => {
-                let doc: OApi = OApi::new(open_api_doc);
-                if let Err(e) = doc.check() {
-                    valid = false;
-                    error!(
-                        "API document {:?} is not valid: {}",
-                        file.file_name().unwrap(),
-                        e
-                    );
-                } else {
-                    let root = doc.root_get().unwrap();
-                    if let Some(file_tags) = root.tags() {
-                        tags.append(&mut file_tags.clone());
-                    }
-                }
-            }
-            Err(e) => {
-                valid = false;
-                error!(
-                    "API document {:?} was not able to be parsed: {}",
-                    file.file_name().unwrap(),
-                    e
-                );
-            }
+  let mut files = find_files(api_path.as_path(), OsStr::new("yml"));
+  files.append(&mut find_files(api_path.as_path(), OsStr::new("yaml")));
+  let mut tags = Vec::new();
+  let mut valid = true;
+  for file in &files {
+    match SparseRoot::new_from_file(PathBuf::from_iter([
+      std::env::current_dir().unwrap(),
+      file.to_path_buf(),
+    ])) {
+      Ok(open_api_doc) => {
+        let doc: OApi = OApi::new(open_api_doc);
+        if let Err(e) = doc.check() {
+          valid = false;
+          error!(
+            "API document {:?} is not valid: {}",
+            file.file_name().unwrap(),
+            e
+          );
+        } else {
+          let root = doc.root_get().unwrap();
+          if let Some(file_tags) = root.tags() {
+            tags.append(&mut file_tags.clone());
+          }
         }
+      }
+      Err(e) => {
+        valid = false;
+        error!(
+          "API document {:?} was not able to be parsed: {}",
+          file.file_name().unwrap(),
+          e
+        );
+      }
+    }
+  }
+
+  if !valid {
+    return Err(anyhow!("Invalid OpenAPI documents"));
+  }
+
+  if tags.len() > 1 {
+    let mut index = 0;
+    while index < tags.len() - 1 {
+      let tag = tags.get(index).unwrap();
+      let mut j = index + 1;
+      while j < tags.len() {
+        let t = tags.get(j).unwrap();
+        if tag.name() == t.name() && tag.description() == t.description() {
+          valid = false;
+          error!(
+            "Duplicate tags: Name: {}\nDescription: {:?}",
+            t.name(),
+            t.description()
+          );
+        }
+        j += 1;
+      }
+      index += 1;
     }
 
     if !valid {
-        return Err(anyhow!("Invalid OpenAPI documents"));
+      return Err(anyhow!("Duplicate tags"));
     }
+  }
 
-    if tags.len() > 1 {
-        let mut index = 0;
-        while index < tags.len() - 1 {
-            let tag = tags.get(index).unwrap();
-            let mut j = index + 1;
-            while j < tags.len() {
-                let t = tags.get(j).unwrap();
-                if tag.name() == t.name() && tag.description() == t.description() {
-                    valid = false;
-                    error!(
-                        "Duplicate tags: Name: {}\nDescription: {:?}",
-                        t.name(),
-                        t.description()
-                    );
-                }
-                j += 1;
-            }
-            index += 1;
-        }
-
-        if !valid {
-            return Err(anyhow!("Duplicate tags"));
-        }
+  if files.len() > 1 {
+    info!("Validating combined OpenAPI documents");
+    let mut files_content = Vec::new();
+    for file in files {
+      files_content.push(open_file(file));
     }
+    let merged_content = merge(files_content);
+    let merged_file = temp_file::with_contents(merged_content.as_bytes());
+    let doc: OApi = OApi::new(
+      SparseRoot::new_from_file(merged_file.path().to_path_buf()).expect("to parse the OpenAPI"),
+    );
 
-    if files.len() > 1 {
-        info!("Validating combined OpenAPI documents");
-        let mut files_content = Vec::new();
-        for file in files {
-            files_content.push(open_file(file));
-        }
-        let merged_content = merge(files_content);
-        let merged_file = temp_file::with_contents(merged_content.as_bytes());
-        let doc: OApi = OApi::new(
-            SparseRoot::new_from_file(merged_file.path().to_path_buf())
-                .expect("to parse the OpenAPI"),
-        );
-
-        doc.check().expect("not to have logic errors");
-        Ok(merged_content)
-    } else {
-        Ok(open_file(files.get(0).unwrap().to_path_buf()))
-    }
+    doc.check().expect("not to have logic errors");
+    Ok(merged_content)
+  } else {
+    Ok(open_file(files.get(0).unwrap().to_path_buf()))
+  }
 }
 
 fn open_file(filename: PathBuf) -> String {
-    let mut file = std::fs::File::open(filename).expect("Couldn't find or open the file");
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)
-        .expect("Couldn't read the contents of the file");
-    contents
+  let mut file = std::fs::File::open(filename).expect("Couldn't find or open the file");
+  let mut contents = String::new();
+  file
+    .read_to_string(&mut contents)
+    .expect("Couldn't read the contents of the file");
+  contents
 }
 
 fn merge(files: Vec<String>) -> String {
-    let mut hash = MergeYamlHash::new();
+  let mut hash = MergeYamlHash::new();
 
-    for file in files {
-        hash.merge(&file);
-    }
+  for file in files {
+    hash.merge(&file);
+  }
 
-    hash.to_string()
+  hash.to_string()
 }
 
 fn find_files(path: &std::path::Path, extension: &OsStr) -> Vec<PathBuf> {
-    let mut files = Vec::new();
-    for entry in path.read_dir().expect("Failed to read directory").flatten() {
-        if entry.path().is_dir() {
-            files.append(&mut find_files(&entry.path(), extension));
-        } else if entry.path().extension() == Some(extension) {
-            files.push(entry.path());
-        }
+  let mut files = Vec::new();
+  for entry in path.read_dir().expect("Failed to read directory").flatten() {
+    if entry.path().is_dir() {
+      files.append(&mut find_files(&entry.path(), extension));
+    } else if entry.path().extension() == Some(extension) {
+      files.push(entry.path());
     }
-    files
+  }
+  files
 }
--- src/util.rs
+++ src/util.rs
@@ -1,63 +1,63 @@
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Default, Clone)]
 pub enum HttpMethod {
-    #[default]
-    Get,
-    Post,
-    Put,
-    Delete,
-    Patch,
-    Head,
-    Options,
-    Trace,
-    Connect,
+  #[default]
+  Get,
+  Post,
+  Put,
+  Delete,
+  Patch,
+  Head,
+  Options,
+  Trace,
+  Connect,
 }
 
 impl From<String> for HttpMethod {
-    fn from(s: String) -> Self {
-        match s.to_lowercase().as_str() {
-            "get" => HttpMethod::Get,
-            "post" => HttpMethod::Post,
-            "put" => HttpMethod::Put,
-            "delete" => HttpMethod::Delete,
-            "patch" => HttpMethod::Patch,
-            "head" => HttpMethod::Head,
-            "options" => HttpMethod::Options,
-            "trace" => HttpMethod::Trace,
-            "connect" => HttpMethod::Connect,
-            _ => panic!("Invalid HTTP method"),
-        }
+  fn from(s: String) -> Self {
+    match s.to_lowercase().as_str() {
+      "get" => HttpMethod::Get,
+      "post" => HttpMethod::Post,
+      "put" => HttpMethod::Put,
+      "delete" => HttpMethod::Delete,
+      "patch" => HttpMethod::Patch,
+      "head" => HttpMethod::Head,
+      "options" => HttpMethod::Options,
+      "trace" => HttpMethod::Trace,
+      "connect" => HttpMethod::Connect,
+      _ => panic!("Invalid HTTP method"),
     }
+  }
 }
 
 impl From<&str> for HttpMethod {
-    fn from(s: &str) -> Self {
-        match s.to_lowercase().as_str() {
-            "get" => HttpMethod::Get,
-            "post" => HttpMethod::Post,
-            "put" => HttpMethod::Put,
-            "delete" => HttpMethod::Delete,
-            "patch" => HttpMethod::Patch,
-            "head" => HttpMethod::Head,
-            "options" => HttpMethod::Options,
-            "trace" => HttpMethod::Trace,
-            "connect" => HttpMethod::Connect,
-            _ => panic!("Invalid HTTP method"),
-        }
+  fn from(s: &str) -> Self {
+    match s.to_lowercase().as_str() {
+      "get" => HttpMethod::Get,
+      "post" => HttpMethod::Post,
+      "put" => HttpMethod::Put,
+      "delete" => HttpMethod::Delete,
+      "patch" => HttpMethod::Patch,
+      "head" => HttpMethod::Head,
+      "options" => HttpMethod::Options,
+      "trace" => HttpMethod::Trace,
+      "connect" => HttpMethod::Connect,
+      _ => panic!("Invalid HTTP method"),
     }
+  }
 }
 
 impl std::fmt::Display for HttpMethod {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            HttpMethod::Get => write!(f, "GET"),
-            HttpMethod::Post => write!(f, "POST"),
-            HttpMethod::Put => write!(f, "PUT"),
-            HttpMethod::Delete => write!(f, "DELETE"),
-            HttpMethod::Patch => write!(f, "PATCH"),
-            HttpMethod::Head => write!(f, "HEAD"),
-            HttpMethod::Options => write!(f, "OPTIONS"),
-            HttpMethod::Trace => write!(f, "TRACE"),
-            HttpMethod::Connect => write!(f, "CONNECT"),
-        }
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    match self {
+      HttpMethod::Get => write!(f, "GET"),
+      HttpMethod::Post => write!(f, "POST"),
+      HttpMethod::Put => write!(f, "PUT"),
+      HttpMethod::Delete => write!(f, "DELETE"),
+      HttpMethod::Patch => write!(f, "PATCH"),
+      HttpMethod::Head => write!(f, "HEAD"),
+      HttpMethod::Options => write!(f, "OPTIONS"),
+      HttpMethod::Trace => write!(f, "TRACE"),
+      HttpMethod::Connect => write!(f, "CONNECT"),
     }
+  }
 }
